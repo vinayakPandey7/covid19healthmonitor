c0aa1e59c5adcd98ed37f95b5cbd0dbe16384bb9
VINAYAK PANDEY <vinayak.pandey779@gmail.com>
2021-11-05 10:49:56 +0530

    .

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -76,18 +76,41 @@ export default class Dashboard extends Component {
       apiService.getLatest()
       .then(response => {
 
-          let tempSeries = [{name: 'temperature',data: []}];
-          let spo2Series = [{name: 'spo2',data: []}];
-          let bpSeries =  [{name: 'bp',data: []}];
+          let tempSeriesData = [];
+          let spo2SeriesData = [];
+          let bpSeriesData =  [];
 
-          console.log(response)
+         
         
-            let tempDate = new Date(elem.date).valueOf()
-            tempTemp.data.push([tempDate, elem.temperature ])
-            spo2Series[0].data.push([tempDate, elem.spo2 ])
-            bpSeries[0].data.push([tempDate, elem.bp ])
-  
-          
+            let tempDate = new Date(response.data.LatestData.date).valueOf()
+            tempSeriesData.push(tempDate, response.data.LatestData.temperature)
+            spo2SeriesData.push(tempDate, response.data.LatestData.spo2)
+            bpSeriesData.push(tempDate, response.data.LatestData.bp)
+
+            // let x = this.state.tempSeries;
+            // x[0].data.push([55,55])
+            // console.log(x)
+
+              
+            this.setState(prevState => ({
+              // userHealthRecord:[...prevState.userHealthRecord,[response.data.LatestData]],
+              tempSeries: [...prevState.tempSeries[0],[22,22]],
+              tempTemp: tempSeriesData ,  
+              tempSpo2: spo2SeriesData,
+              tempBp: bpSeriesData
+              // spo2Series[0].data: [...prevState.spo2Series, spo2SeriesData],
+              // bpSeries:  [...prevState.bpSeries, bpSeriesData]
+            }))
+
+
+
+
+
+
+
+            // console.log(tempSeriesData)
+            console.log(this.state)
+            // console.log(bpSeriesData)
       //  this.setState({
       //     userHealthRecord: response.data.healthdata,
       //     tempSeries: tempSeries,
@@ -119,12 +142,12 @@ export default class Dashboard extends Component {
                 load: function () {
     
                     // set up the updating of the chart each second
-                    // var series = this.series[0];
-                    // setInterval(function () {
-                    //     var x = (new Date()).getTime(), // current time
-                    //         y = Math.round(Math.random() * 100);
-                    //     series.addPoint([x, y], true, true);
-                    // }, 1000);
+                    var series = this.series[0];
+                    setInterval(function () {
+                        // var x = (new Date()).getTime(), // current time
+                        //     y = Math.round(Math.random() * 100);
+                        series.addPoint(that.state.tempTemp, true, true);
+                    }, 4000);
                     console.log(that.state.tempSeries)
                 }
             }
