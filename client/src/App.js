cbb1afe9fb6c08a88824a558450b26cb3043ac26
vinayakpandey7 <vinayak.pandey779@gmail.com>
2021-11-10 03:32:50 +0530

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 import React, {useState, useEffect, Component} from 'react';
 import {
   BrowserRouter as Router,
